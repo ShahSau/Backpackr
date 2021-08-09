025b469ed121836ca55bdbb9513ee3fe3f689471
ShahSau <shahriarksaurov@gmail.com>
2021-08-10 01:01:52 +0300

    List

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,48 @@
 import React, { useState, useEffect, createRef } from 'react';
-
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+import useStyles from './styles.js';
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
 
 const List = () => {
- 
-
+    const classes = useStyles();
+    const [type, setType] = useState('restaurant')
+    const [rating, setRating] = useState('')
+    const places= [
+        {name:'coollll1'},
+        {name:'coollll3'},
+        {name:'coollll2'},
+        {name:'coollll1'},
+        {name:'coollll3'},
+        {name:'coollll2'},
+    
+    ]
   return (
-    <div>
-        <h1>LIST</h1>
+    <div className={classes.container}>
+        <Typography variant="h4">Food & Dining around you</Typography>
+        <FormControl className={classes.formControl}>
+            <InputLabel id="type">Type</InputLabel>
+            <Select value={type} onChange={(e)=> setType(e.target.value)}>
+              <MenuItem value="restaurants">Restaurants</MenuItem>
+              <MenuItem value="hotels">Hotels</MenuItem>
+              <MenuItem value="attractions">Attractions</MenuItem>
+            </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+            <InputLabel>Rating</InputLabel>
+            <Select value={rating} onChange={(e)=> setRating(e.target.value)}>
+              <MenuItem value={0}>All</MenuItem>
+              <MenuItem value={3}>Above 3.0</MenuItem>
+              <MenuItem value={4}>Above 4.0</MenuItem>
+              <MenuItem value={4.5}>Above 4.5</MenuItem>
+            </Select>
+        </FormControl>
+        <Grid container spacing={3} className={classes.list}>
+            {places?.map((place, i) => (
+              <Grid key={i} item xs={12}>
+                <PlaceDetails place={place}/>
+              </Grid>
+            ))}
+        </Grid>
     </div>
   );
 };
