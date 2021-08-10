9fc1d0836d3c56d9fa1cbb6f536a1f0a1ea7ec1f
ShahSau <shahriarksaurov@gmail.com>
2021-08-11 01:43:46 +0300

    color fix

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -11,7 +11,7 @@ export default makeStyles((theme) => ({
     position: 'relative',
     borderRadius: theme.shape.borderRadius,
     backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': { backgroundColor: alpha(theme.palette.common.white, 0.25) },
+    '&:hover': { backgroundColor: alpha(theme.palette.primary.dark, 0.25) },
     marginRight: theme.spacing(2),
     marginLeft: 0,
     width: '100%',
@@ -27,6 +27,6 @@ export default makeStyles((theme) => ({
     padding: theme.spacing(1, 1, 1, 0), paddingLeft: `calc(1em + ${theme.spacing(4)}px)`, transition: theme.transitions.create('width'), width: '100%', [theme.breakpoints.up('md')]: { width: '20ch' },
   },
   toolbar: {
-    display: 'flex', justifyContent: 'space-between',
+    display: 'flex', justifyContent: 'space-between', backgroundColor: '#29446b'
   },
 }));
