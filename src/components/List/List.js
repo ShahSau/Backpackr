e9b638e20a42c45b3cde86b5f15d24f47a5356cf
ShahSau <shahriarksaurov@gmail.com>
2021-08-11 17:04:12 +0300

    build

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -37,9 +37,9 @@ const List = ({places, type, setType, rating, setRating, childClicked, isLoading
               <InputLabel id="rating">Rating</InputLabel>
               <Select id="rating" value={rating} onChange={(e) => setRating(e.target.value)}>
                 <MenuItem value="">All</MenuItem>
+                <MenuItem value="2">Above 2.0</MenuItem>
                 <MenuItem value="3">Above 3.0</MenuItem>
                 <MenuItem value="4">Above 4.0</MenuItem>
-                <MenuItem value="4.5">Above 4.5</MenuItem>
               </Select>
             </FormControl>
             <Grid container spacing={3} className={classes.list}>
