16fb36994b5e77ff657dbf155eed03b25c1b3653
shahriar <shahriarksaurov@gmail.com>
2021-10-25 01:55:49 +0300

    indentation

--- src/api/index.js
+++ src/api/index.js
@@ -1,20 +1,26 @@
 /* eslint-disable consistent-return */
-import axios from 'axios';
+import axios from "axios";
 
 export const getPlacesData = async (type, sw, ne) => {
   try {
-    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
-      params: {
-        bl_latitude: sw.lat,
-        bl_longitude: sw.lng,
-        tr_longitude: ne.lng,
-        tr_latitude: ne.lat,
-      },
-      headers: {
-        'x-rapidapi-key': '09cfa80fdfmshfab9bb2e6524034p10409ejsn8327b13fb216',
-        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-      },
-    });
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+          tr_latitude: ne.lat,
+        },
+        headers: {
+          "x-rapidapi-key":
+            "09cfa80fdfmshfab9bb2e6524034p10409ejsn8327b13fb216",
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+        },
+      }
+    );
 
     return data;
   } catch (error) {
@@ -25,13 +31,17 @@ export const getPlacesData = async (type, sw, ne) => {
 export const getWeatherData = async (lat, lng) => {
   try {
     if (lat && lng) {
-      const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
-        params: { lat: lat, lon: lng },
-        headers: {
-          'x-rapidapi-key': '09cfa80fdfmshfab9bb2e6524034p10409ejsn8327b13fb216',
-          'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-        },
-      });
+      const { data } = await axios.get(
+        "https://community-open-weather-map.p.rapidapi.com/find",
+        {
+          params: { lat: lat, lon: lng },
+          headers: {
+            "x-rapidapi-key":
+              "09cfa80fdfmshfab9bb2e6524034p10409ejsn8327b13fb216",
+            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+          },
+        }
+      );
 
       return data;
     }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,58 +1,89 @@
-import React, { useState, useEffect, createRef } from 'react';
-import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
-import useStyles from './styles.js';
-import PlaceDetails from '../PlaceDetails/PlaceDetails';
+import React, { useState, useEffect, createRef } from "react";
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+import useStyles from "./styles.js";
+import PlaceDetails from "../PlaceDetails/PlaceDetails";
 
-const List = ({places, type, setType, rating, setRating, childClicked, isLoading}) => {
-    const classes = useStyles();
-    
+const List = ({
+  places,
+  type,
+  setType,
+  rating,
+  setRating,
+  childClicked,
+  isLoading,
+}) => {
+  const classes = useStyles();
 
-    // if we put what we want to print in {} it will print it with details. if value is 5, with out {} it will print 5
-    // but with {} it will print childCliked: 5
-    console.log({childClicked})
-    const [elRefs, setElRefs] = useState([]);
-    useEffect(()=>{ 
-      // this will create an array of the places and i indicate the clicked place
-      setElRefs((refs) => Array(places.length).fill().map((_, i) => refs[i] || createRef()));
-  }, [places]);
-   
-    return (
-      <div className={classes.container}>
-        <Typography variant="h4">Food & Dining around you</Typography>
-        {isLoading ? (
-          <div className={classes.loading}>
-            <CircularProgress size="5rem" />
-          </div>
-        ) : (
-          <>
-            <FormControl className={classes.formControl}>
-              <InputLabel id="type">Type</InputLabel>
-              <Select id="type" value={type} onChange={(e) => setType(e.target.value)}>
-                <MenuItem value="restaurants">Restaurants</MenuItem>
-                <MenuItem value="hotels">Hotels</MenuItem>
-                <MenuItem value="attractions">Attractions</MenuItem>
-              </Select>
-            </FormControl>
-            <FormControl className={classes.formControl}>
-              <InputLabel id="rating">Rating</InputLabel>
-              <Select id="rating" value={rating} onChange={(e) => setRating(e.target.value)}>
-                <MenuItem value="">All</MenuItem>
-                <MenuItem value="2">Above 2.0</MenuItem>
-                <MenuItem value="3">Above 3.0</MenuItem>
-                <MenuItem value="4">Above 4.0</MenuItem>
-              </Select>
-            </FormControl>
-            <Grid container spacing={3} className={classes.list}>
-              {places?.map((place, i) => (
-                <Grid ref={elRefs[i]} key={i} item xs={12}>
-                  <PlaceDetails selected={Number(childClicked) === i} refProp={elRefs[i]} place={place} />
-                </Grid>
-              ))}
-            </Grid>
-          </>
-        )}
-      </div>
+  // if we put what we want to print in {} it will print it with details. if value is 5, with out {} it will print 5
+  // but with {} it will print childCliked: 5
+  console.log({ childClicked });
+  const [elRefs, setElRefs] = useState([]);
+  useEffect(() => {
+    // this will create an array of the places and i indicate the clicked place
+    setElRefs((refs) =>
+      Array(places.length)
+        .fill()
+        .map((_, i) => refs[i] || createRef())
     );
-  };
-  
-  export default List;
+  }, [places]);
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">Food & Dining around you</Typography>
+      {isLoading ? (
+        <div className={classes.loading}>
+          <CircularProgress size="5rem" />
+        </div>
+      ) : (
+        <>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="type">Type</InputLabel>
+            <Select
+              id="type"
+              value={type}
+              onChange={(e) => setType(e.target.value)}
+            >
+              <MenuItem value="restaurants">Restaurants</MenuItem>
+              <MenuItem value="hotels">Hotels</MenuItem>
+              <MenuItem value="attractions">Attractions</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="rating">Rating</InputLabel>
+            <Select
+              id="rating"
+              value={rating}
+              onChange={(e) => setRating(e.target.value)}
+            >
+              <MenuItem value="">All</MenuItem>
+              <MenuItem value="2">Above 2.0</MenuItem>
+              <MenuItem value="3">Above 3.0</MenuItem>
+              <MenuItem value="4">Above 4.0</MenuItem>
+            </Select>
+          </FormControl>
+          <Grid container spacing={3} className={classes.list}>
+            {places?.map((place, i) => (
+              <Grid ref={elRefs[i]} key={i} item xs={12}>
+                <PlaceDetails
+                  selected={Number(childClicked) === i}
+                  refProp={elRefs[i]}
+                  place={place}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default List;
